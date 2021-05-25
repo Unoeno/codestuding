a2e46908518f0656ef10b6df0ba64ff15bc4c874
Unoeno <84673149+Unoeno@users.noreply.github.com>
2021-05-25 13:47:29 +0900

    Update bitcoinAutoTradeWithAI.py

--- bitcoinAutoTradeWithAI.py
+++ bitcoinAutoTradeWithAI.py
@@ -3,18 +3,9 @@ import pyupbit
 import datetime
 import schedule
 from fbprophet import Prophet
-import requests
 
 access = "TxNnyjpzOxGpOUX5V7khlsIKGL2p5mlJcWKe7TQK"
 secret = "VgIafGmtxo2JtXyqIzkFNKkxb807B7Y0miBE7zKY"
-myToken = "xoxb-2092150365030-2084161292615-pNUbeUxjYr1bJ07LsjY2bZKK"
-
-def post_message(token, channel, text):
-    """슬랙 메시지 전송"""
-    response = requests.post("https://slack.com/api/chat.postMessage",
-        headers={"Authorization": "Bearer "+token},
-        data={"channel": channel,"text": text}
-    )
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
@@ -67,8 +58,6 @@ schedule.every().hour.do(lambda: predict_price("KRW-BTC"))
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
-# 시작 메세지 슬랙 전송
-post_message(myToken,"#studing", "autotrade start")
 
 # 자동매매 시작
 while True:
@@ -85,15 +74,11 @@ while True:
                 krw = get_balance("KRW")
                 if krw > 5000:
                     upbit.buy_market_order("KRW-BTC", krw*0.9995)
-                    post_message(myToken,"#studing", "BTC buy : " +str(buy_result))
-
         else:
             btc = get_balance("BTC")
             if btc > 0.00008:
                 upbit.sell_market_order("KRW-BTC", btc*0.9995)
-                post_message(myToken,"#studing", "BTC buy : " +str(sell_result))
         time.sleep(1)
     except Exception as e:
         print(e)
-        post_message(myToken,"#studing", e)
         time.sleep(1)
